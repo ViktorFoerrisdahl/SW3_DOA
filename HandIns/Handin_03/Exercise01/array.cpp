d318b14a650f463014ea9c7226c92c6fa2e4b629
Viktor Førrisdahl <viktorkirkeby15@gmail.com>
2026-09-24 11:04:44 +0200

    sidste finpudsning på opgave 1

--- HandIns/Handin_03/Exercise01/array.cpp
+++ HandIns/Handin_03/Exercise01/array.cpp
@@ -16,6 +16,9 @@ bool search(int *arr, int size, int target)
         return true;
     }
 
+    // Til step 4 test:
+    // std::cout << "arr[0] = " << arr[0] << " size = " << size << std::endl;
+
     return search(arr + 1, size - 1, target);
 }
 
@@ -30,6 +33,9 @@ int max(int *arr, int size, int currentMax)
     {
         currentMax = arr[0];
     }
+
+    // Til step 4 test:
+    std::cout << "arr[0] = " << arr[0] << " size = " << size << " currentMax: " << currentMax << std::endl;
 
     return max(arr + 1, size - 1, currentMax);
 }
@@ -46,28 +52,30 @@ int min(int *arr, int size, int currentMin)
         currentMin = arr[0];
     }
 
+    // Til step 4 test:
+    std::cout << "arr[0] = " << arr[0] << " size = " << size << " currentMin: " << currentMin << std::endl;
+
     return min(arr + 1, size - 1, currentMin);
 }
 
 
 int main()
 {
-
     int array[10] = {-89, 1, 2, 3, 4, 200, 200000, 213123, 132323, 10000000};
 
-    std::cout << "findes tallet " << TAL_VI_LEDER_EFTER << " i vores array? " << std::boolalpha << search(array, std::size(array) , TAL_VI_LEDER_EFTER);
+    // std::cout << "findes tallet " << TAL_VI_LEDER_EFTER << " i vores array? " << std::endl << std::boolalpha << search(array, std::size(array) , TAL_VI_LEDER_EFTER);
 
-    std::cout << std::endl;
+    // std::cout << std::endl;
 
-    std::cout << "findes tallet " << TAL2_VI_LEDER_EFTER << " i vores array? " << std::boolalpha << search(array, std::size(array) , TAL2_VI_LEDER_EFTER);
+    // std::cout << "findes tallet " << TAL2_VI_LEDER_EFTER << " i vores array? " << std::boolalpha << search(array, std::size(array) , TAL2_VI_LEDER_EFTER);
 
-    std::cout << std::endl;
+    // std::cout << std::endl;
 
-    std::cout << "Max i vores array er:  " << max(array, std::size(array), array[0]);
+    std::cout << "Max i vores array er:  " << std::endl << max(array, std::size(array), array[0]);
 
-    std::cout << std::endl;
+    // std::cout << std::endl;
 
-    std::cout << "Min i vores array er:  " << min(array, std::size(array), array[0]);
+    // std::cout << "Min i vores array er:  " << min(array, std::size(array), array[0]);
 
     return 0;
 }
